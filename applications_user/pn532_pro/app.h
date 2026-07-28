1cf6360a8f305854faf3f98ae6c66e7a56dd684c
minhlam02012014-a11y <minhlam02012014@gmail.com>
2026-07-28 22:48:57 +0700

    Update app.h

--- applications_user/pn532_pro/app.h
+++ applications_user/pn532_pro/app.h
@@ -1,11 +1,19 @@
 #pragma once
 
 #include <furi.h>
+#include <furi_hal.h>
 #include <gui/gui.h>
+#include <gui/view_dispatcher.h>
 #include <gui/modules/submenu.h>
 #include <gui/modules/widget.h>
 #include <notification/notification_messages.h>
+
 #include "pn532_cmd.h"
+
+typedef enum {
+    Pn532AppViewSubmenu,
+    Pn532AppViewWidget,
+} Pn532AppView;
 
 typedef struct {
     ViewDispatcher* view_dispatcher;
@@ -14,8 +22,3 @@ typedef struct {
     NotificationApp* notification;
     Pn532Dev* dev;
 } Pn532App;
-
-typedef enum {
-    Pn532AppViewSubmenu,
-    Pn532AppViewWidget,
-} Pn532AppView;
