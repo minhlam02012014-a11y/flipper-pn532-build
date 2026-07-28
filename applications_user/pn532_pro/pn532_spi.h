a988a30446247cbd318cb588e1c74533b7f50649
minhlam02012014-a11y <minhlam02012014@gmail.com>
2026-07-28 17:41:10 +0700

    Create pn532_spi.h for PN532 SPI interface
    Add PN532 SPI interface header file with function declarations and constants.

---
+++ applications_user/pn532_pro/pn532_spi.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <furi.h>
+#include <furi_hal.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#define PN532_SPI_STATREAD  (0x02)
+#define PN532_SPI_DATAWRITE (0x01)
+#define PN532_SPI_DATAREAD  (0x03)
+#define PN532_SPI_READY     (0x01)
+
+typedef struct {
+    const GpioPin* cs_pin;
+    const GpioPin* rst_pin;
+} Pn532Spi;
+
+Pn532Spi* pn532_spi_alloc(void);
+void pn532_spi_free(Pn532Spi* instance);
+void pn532_spi_init(Pn532Spi* instance);
+void pn532_spi_reset(Pn532Spi* instance);
+bool pn532_spi_wait_ready(Pn532Spi* instance, uint32_t timeout_ms);
+bool pn532_spi_write_frame(Pn532Spi* instance, const uint8_t* data, uint16_t len);
+bool pn532_spi_read_frame(Pn532Spi* instance, uint8_t* buffer, uint16_t len, uint32_t timeout_ms);
